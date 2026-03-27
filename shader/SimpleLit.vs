982143a5380f9e88e668cd085d91fed8ebefdffe
feiqi3 <zjgtzn@outlook.com>
2026-03-27 16:02:51 +0800

    feat: simple demo scene update - gltf load.

--- shader/SimpleLit.vs
+++ shader/SimpleLit.vs
@@ -1,8 +1,34 @@
+#version 450
 #include "CommonSets.inl"
 
 #include "PBREntity.h"
 
-layout(set = 2, binding = 1) uniform UniformBufferObject {
-    PBRData pbrData
+layout(set = 3, binding = 1) uniform MaterialData {
+    PBRData pbrData;
 } CBUFFER_pbrData;
 
+layout(location = 0) in vec3 i_pos;         
+layout(location = 1) in vec3 i_normal;           
+layout(location = 2) in vec2 i_texcoord0;        
+layout(location = 3) in vec2 i_texcoord1;        
+layout(location = 4) in vec4 i_color;            
+
+layout(location = 0) out vec3 o_worldPos;
+layout(location = 1) out vec3 o_normal;
+layout(location = 2) out vec2 o_texcoord0;
+layout(location = 3) out vec2 o_texcoord1;
+layout(location = 4) out vec3 o_color;
+layout(location = 5) out vec3 o_viewDir;
+
+void main(){
+    vec4 worldPos =  OBJDATA.worldMatrix * vec4(i_pos,1.f);
+    o_worldPos       = worldPos.xyz;
+    vec3 worldNormal = normalize(mat3(OBJDATA.invWorldMatrix) * i_normal);
+    o_normal         = worldNormal;
+    vec3 viewDir     = o_worldPos - CAMDATA.CameraPosition.xyz;
+    o_viewDir = viewDir;
+    o_texcoord0 = i_texcoord0;
+    o_texcoord1 = i_texcoord1;
+    o_color = i_color.rgb;
+    gl_Position = CAMDATA.MatViewProj * worldPos;
+}
--- src/Renderer/NaiveScene.cpp
+++ src/Renderer/NaiveScene.cpp
@@ -25,18 +25,41 @@ namespace Render {
 		float radius;
 	};
 	
+	class SpinComponent : public Component {
+	public:
+		SpinComponent()
+		{
+
+		}
+
+		virtual void onUpdate(float deltaTime)override {
+			auto obj = this->owner();
+			float speed = 0.05;
+			xDeg += speed * deltaTime;
+			owner()->setLocalRotation(fromAxisAngle(vec3(0,1,0), xDeg));
+		};
+
+	private:
+		float xDeg = 0;
+		int t = 0;
+	};
+
+
 	Scene* Render::createSceneByCode()
 	{
 		Scene* naiveScene = new Scene();
-		auto objectA = naiveScene->createObject("CubeObject1");
-		objectA->addComponent<SimpleRenderComponent>();
-		objectA->setLocalPosition(vec3(0, 0, -2));
-		auto objectB = naiveScene->createObject("CubeObject2");
-		objectB->addComponent<SimpleRenderComponent>();
-		objectB->addComponent<MoveComponent>( vec3(0,0,-5),3 );
+		//auto objectA = naiveScene->createObject("CubeObject1");
+		//objectA->addComponent<SimpleRenderComponent>();
+		//objectA->setLocalPosition(vec3(0, 0, -2));
+		//auto objectB = naiveScene->createObject("CubeObject2");
+		//objectB->addComponent<SimpleRenderComponent>();
+		//objectB->addComponent<MoveComponent>( vec3(0,0,-5),3 );
 		GLTFLoader loader;
-		auto model = loader.createFromFilePath("../resources/Fox/glTF/Fox.gltf");
-		loader.toEngineSceneNode(naiveScene, model);
+		auto model = loader.createFromFilePath("../resources/Fox/fox.gltf");
+		auto node = loader.toEngineSceneNode(naiveScene, model);
+		node->setLocalScale(vec3(0.025, 0.025, 0.025));
+		node->setLocalPosition(vec3(0,-25, -50));
+		node->addComponent<SpinComponent>();
 		delete model;
 		return naiveScene;
 	}
